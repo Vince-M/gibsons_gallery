cb128b4990a7ba45bad31e07f9595bfb2b50bd06
Vince Mendella <vmendella@gmail.com>
2025-04-29 19:08:30 -0500

    add header and footer files

---
+++ footer.php
@@ -0,0 +1,54 @@
+  <?php wp_footer(); ?>
+  
+  <!-- FOOTER
+  =================================================== -->
+  <footer id="footer" class="footer" role="contentinfo">
+    <div class="wrapper footer__content">
+      <div class="footer__branding">
+        <a href="index.html">
+          <img src="img/footer__branding.svg" alt="" width="175" height="95">
+        </a>
+      </div>
+      <div class="footer__nav">
+        <div>
+          <ul class="footer__menu">
+            <li><a href="artists.html">Artists</a></li>
+            <li><a href="workshops.html">Workshops</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="About.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div>
+        <div class="footer__contactInfo">
+          <div class="footer__info">
+            <p><strong>Visit Us</strong><br>
+            170 Saffie Road<br>
+            Albert Beach, MB R0E 2A0</p>
+            
+            <p><strong>Gallery Hours</strong><br>
+            Thursday to Sunday<br>
+            10am-4pm or by appointment</p>
+          </div>
+          <div class="footer__connect">
+            <p><strong>Connect With Us</strong><br>
+            (204)297-3979</p>
+            <div class="footer__social">
+              <img src="img/insta_icon.svg" alt="" width="27" height="27">
+              <img src="img/facebook_icon.svg" alt="" width="15" height="30">
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="wrapper copyright">
+      <p>© 2024 Gibson's Glass & Art. All rights reserved. | <a href="privacy.html">Privacy Policy</a></p>
+      <p>Photography by Santi Ardila</p>
+    </div>
+    
+  </footer>
+  <!-- ============================================== -->
+</body>
+</html>
