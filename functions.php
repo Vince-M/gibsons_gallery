0cd1e3b3839b3a6eb7966c28887fbc536d06ce7e
Vince Mendella <vmendella@gmail.com>
2025-06-02 06:16:38 -0500

    add defer partsing js to functions

--- functions.php
+++ functions.php
@@ -35,3 +35,10 @@ function gibsons_gallery_features() {
 add_action( 'after_setup_theme', 'gibsons_gallery_features' );
 
 
+function defer_parsing_of_js( $url ) {
+  if ( is_user_logged_in() ) return $url; //don't break WP Admin
+  if ( FALSE === strpos( $url, '.js' ) ) return $url;
+  if ( strpos( $url, 'jquery.js' ) ) return $url;
+  return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
